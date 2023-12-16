cca050f46626e5d323450d54fcaa2bba589e54aa
serrriy <sergon2002@gmail.com>
2023-12-16 15:23:35 +0300

    Initial Commit

---
+++ GC/GCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCApp.swift
+//  GC
+//
+//  Created by Sergey Goncharov on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
